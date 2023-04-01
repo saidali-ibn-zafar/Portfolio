e96f93eb1ccaacf51145d7101ec896ef22c30935
saidali_ibn_zafar <120341849+saidali-ibn-zafar@users.noreply.github.com>
2023-04-02 01:07:42 +0200

    Add files via upload

---
+++ JS_CSS_Portfolio-Project/app.js
@@ -0,0 +1,36 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+
+function PageTransition(){
+    // button click active class
+    for(let i = 0; i < sectBtn.length; i++){
+        sectBtn[i].addEventListener('click', function(){
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            this.className += ' active-btn';
+        })
+    }
+
+    // sections active 
+    allSections.addEventListener('click', (e)=>{
+        const id = e.target.dataset.id;
+        if(id){
+            // remove selected from the other btns
+            sectBtns.forEach((btn) => {
+                btn.classList.remove('active');
+            })
+            e.target.classList.add('active');
+
+            // hide other sections 
+            sections.forEach((section) => {
+                section.classList.remove('active');
+            })
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
+}
+
+PageTransition();
